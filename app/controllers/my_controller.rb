daaad2e45a536dd71698af6377e0b182ae589c7d
Dan Ahern <danahern@gmail.com>
2012-12-07 00:37:20 -0500

    Starred repos now automatically update

--- app/controllers/my_controller.rb
+++ app/controllers/my_controller.rb
@@ -1,5 +1,9 @@
 class MyController < ApplicationController
   def starred_repos
     @starred_repos = authenticated_github.activity.starring.starred
+    respond_to do |format|
+      format.js
+      format.html
+    end
   end
 end
--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -4,10 +4,4 @@ class SearchController < ApplicationController
       @repos = github.search.repositories(keyword: params[:keywords], language: params[:language])
     end
   end
-
-  def users
-    if params[:user_name]
-      @starred = github.activity.starring.starred(user: params[:user_name])
-    end
-  end
 end
